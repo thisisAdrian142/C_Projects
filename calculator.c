afea6da5667c31ab89783a4386a13a4c94b20083
thisisAdrian142 <EmailAdrian.png@gmail.com>
2024-01-22 03:04:52 +1100

    finish the coding

---
+++ calculator.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    char ch;
+    double a,b;
+    while(1) {
+        printf("Enter an operator (+, -, *, /), "
+                "if want to exist press x: ");
+        scanf(" %c", &ch);
+
+        if (ch == 'x') {
+            exit(0);
+        } else {
+            printf("Enter two first and second operand: ");
+            scanf("%lf %lf", &a, &b);
+        }
+        switch(ch) {
+            case '+':
+            printf("%.2lf + %.2lf = %.2lf\n", a, b, a+b);
+            break;
+
+            case '-':
+            printf("%.2lf - %.2lf = %.2lf\n", a, b, a-b);
+            break;
+
+            case '*':
+            printf("%.2lf * %.2lf = %.2lf\n", a, b, a*b);
+            break;
+
+            case '/':
+            printf("%.2lf / %.2lf = %.2lf\n", a, b, a/b);
+            break;
+
+            default:
+            printf("Error! Please write a valid operator\n");
+        }
+        printf("\n");
+
+    }
+}
